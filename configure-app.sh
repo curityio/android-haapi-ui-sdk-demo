8baf45a7cfc50245f232ffebf936a0ff2bdae821
gary.archer <gary.archer@WORKMAC.local>
2023-12-07 16:01:12 +0100

    Deployment updates

---
+++ configure-app.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+####################################################
+# When using ngrok, this makes configuration updates
+####################################################
+
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
+BASE_URL='https://localhost:8443'
+
+#
+# Update URLs referenced in the code example to match NGROK
+#
+function replaceTextInFile() {
+
+  FROM="$1"
+  TO="$2"
+  FILE="$3"
+  
+  if [ "$(uname -s)" == 'Darwin' ]; then
+    sed -i '' "s/$FROM/$TO/g" "$FILE"
+  else
+    sed -i -e "s/$FROM/$TO/g" "$FILE"
+  fi
+}
+
+RUNTIME_BASE_URL=$(curl -s http://localhost:4040/api/tunnels | jq -r '.tunnels[] | select(.proto == "https") | .public_url')
+if [ $RUNTIME_BASE_URL != '' ]; then
+
+  RUNTIME_BASE_URL=${RUNTIME_BASE_URL//\//\\/}
+  
+  # Override the configuration URL to enable internet connectivity from any device
+  replaceTextInFile $DEFAULT_URL $RUNTIME_BASE_URL './app/src/main/java/io/curity/haapidemo/Configuration.kt'
+  
+  # Override the asset statements URL to enable passkeys to work
+  replaceTextInFile $DEFAULT_URL $RUNTIME_BASE_URL './app/src/main/res/values/strings.xml'
+fi
