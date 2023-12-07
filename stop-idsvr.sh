99d95a660ccd197ec75c34c38ab8409da3d9116f
gary.archer <gary.archer@WORKMAC.local>
2023-12-07 10:19:14 +0100

    Deployment updates

--- stop-idsvr.sh
+++ stop-idsvr.sh
@@ -6,4 +6,25 @@
 
 USE_NGROK='false'
 EXAMPLE_NAME='haapi'
+
+#
+# Download mobile deployment resources
+#
+rm -rf deployment
+git clone https://github.com/curityio/mobile-deployments deployment
+if [ $? -ne 0 ]; then
+  echo 'Problem encountered downloading deployment resources'
+  exit
+fi
+
+#
+# TODO: delete after merge
+#
+cd deployment
+git checkout feature/passkeys
+cd ..
+
+#
+# Do the teardown
+#
 ./deployment/stop.sh "$USE_NGROK" "$EXAMPLE_NAME"
