80b95c0070c02b32d4c202a16097c888b67fa1b3
gary.archer <gary.archer@WORKMAC.local>
2023-12-07 09:10:53 +0100

    Deployment updates

--- stop-idsvr.sh
+++ stop-idsvr.sh
@@ -4,6 +4,6 @@
 # Free deployment resources when finished with the code example
 ###############################################################
 
-USE_NGROK=false
+USE_NGROK='false'
 EXAMPLE_NAME='haapi'
 ./deployment/stop.sh "$USE_NGROK" "$EXAMPLE_NAME"
