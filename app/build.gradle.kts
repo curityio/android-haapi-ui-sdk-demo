1432a105d72076799716d1a2377621e175489df9
gary.archer <gary.archer@curity.io>
2025-04-14 18:38:19 +0200

    Update to Android 35 SDK and add copyrights to recent source files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,13 +22,13 @@ plugins {
 }
 
 android {
-    compileSdk = 34
+    compileSdk = 35
     namespace = "io.curity.haapidemo"
 
     defaultConfig {
         applicationId = "io.curity.haapidemo"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
     }
